d695231663f5ad08a374e4cb9153053b50176350
Seok99 <ydbtjrqja@naver.com>
2026-02-21 15:21:52 +0900

    구상도

--- Automatic Driving.py
+++ Automatic Driving.py
@@ -71,3 +71,6 @@ def color_diff(a,b): #목표 색상과 오차 비교
 
 def process_camera_image(cam: Camera):
     REF = (67,64,64)#Reference(목표) RGB(64,64,67):아주어두운 회색
+    yellow_RGB = (95, 187, 203)
+    white_RGB = ()#측정 필요
+    
