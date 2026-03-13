7b1d2864c0b486e708fc8b814b4233eb09d23ae9
Seok99 <ydbtjrqja@naver.com>
2026-03-13 17:00:24 +0900

    openCV흰색 검출

--- ex.py
+++ ex.py
@@ -1,33 +1,47 @@
 from vehicle import Driver
 import numpy as np
+import cv2
 
 driver = Driver()
-timestep = int(driver.getBasicTimeStep())
 
-camera = driver.getDevice("camera")
-camera.enable(timestep)
+camera = driver.getDevice("camera_left")
+camera.enable(32)
 
 width = camera.getWidth()
 height = camera.getHeight()
+
+speed = 0
 
 while driver.step() != -1:
 
     image = camera.getImage()
 
     img = np.frombuffer(image, np.uint8).reshape((height, width, 4))
-    img = img[:, :, :3]   # RGBA → RGB
+    img = img[:, :, :3]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-    # 화면 중앙 픽셀
-    r, g, b = img[height//2][width//2]
+    roi = img[int(height*0.3):height, :]
 
-    if r > 200 and g > 200 and b > 200:
-        print("True - White line detected")
+    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
-        # 차량 출발
-        driver.setCruisingSpeed(20)
+    # 흰색 검출
+    lower_white = np.array([0,0,180])
+    upper_white = np.array([180,60,255])
 
+    mask = cv2.inRange(hsv, lower_white, upper_white)
+
+    white_pixels = cv2.countNonZero(mask)
+    
+
+    if white_pixels > 26:
+        speed = 20
     else:
-        print("False")
+        speed = 0
 
-        # 차량 정지
-        driver.setCruisingSpeed(0)
+    driver.setCruisingSpeed(speed)
+
+    cv2.imshow("roi", roi)
+    cv2.imshow("white mask", mask)
+
+    cv2.waitKey(1)
+    print("white_pixels:", white_pixels)
